bb575682c2476d763da06b3210e296a04a3c9b2b
ryptix <baptistethi@gmail.com>
2020-08-26 23:04:32 +0200

    Update code.h

--- code.h
+++ code.h
@@ -1,1 +1,8 @@
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#pragma comment(lib,"user32.lib") 
 
+int main() {
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, "C:\\Users\\bapti\\Pictures\\Test.bmp", SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+}
